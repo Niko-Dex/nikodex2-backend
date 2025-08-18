53d86e6d01f809f9be95c0823959d9197fb1143f
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-08-18 14:42:42 +0700

    change backend to use sessionmaker instead of one Session

--- service.py
+++ service.py
@@ -2,7 +2,7 @@ import os
 import datetime
 from dotenv import load_dotenv
 from models import Niko, Ability, Blog, User
-from sqlalchemy.orm import Session, selectinload
+from sqlalchemy.orm import Session, selectinload, sessionmaker
 from sqlalchemy import and_, create_engine, text, select, insert, func
 import dto
 
@@ -12,40 +12,53 @@ connection_str = "mysql+mysqlconnector://{}:{}@{}:{}/{}" \
     .format(os.environ['MYSQL_USER'], os.environ['MYSQL_PASS'], os.environ['MYSQL_URI'], os.environ['MYSQL_PORT'], "nikodex")
 
 engine = create_engine(connection_str, echo=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
-session = Session(engine)
+def run_in_session(func):
+    def wrapper(*args, **kwargs):
+        session = SessionLocal()
+        try:
+            result = func(session, *args, **kwargs)
+            return result
+        except:
+            print("buh")
+            session.rollback()
+            return None
+        finally:
+            session.close()
+    return wrapper
 
-def get_all():
+@run_in_session
+def get_all(session):
     stmt = select(Niko).options(selectinload(Niko.abilities))
     return session.scalars(stmt).fetchall()
 
-def get_by_name(name: str):
+@run_in_session
+def get_by_name(session, name: str):
     stmt = select(Niko).options(selectinload(Niko.abilities)).where(Niko.name.like(name))
     return session.scalars(stmt).fetchall()
 
-def get_niko_by_id(id: int):
-    try:
-        stmt = select(Niko).options(selectinload(Niko.abilities)).where(Niko.id == id)
-        res = session.scalars(stmt).one()
-        return res
-    except:
-        return None
+@run_in_session
+def get_niko_by_id(session, id: int):
+    stmt = select(Niko).options(selectinload(Niko.abilities)).where(Niko.id == id)
+    res = session.scalars(stmt).one()
+    return res
 
-def get_nikos_count():
+@run_in_session
+def get_nikos_count(session):
     return session.query(func.count(Niko.id)).one()[0]
 
-def insert_niko(req: dto.NikoRequest):
-    try:
-        stmt = insert(Niko).values(name=req.name, description=req.description,
-        image=req.image, doc="", author=req.author, full_desc=req.full_desc)
-    
-        result = session.execute(stmt)
-        session.commit()
-        return {"msg":"Inserted Niko."}
-    except:
-        return None
-    
-def update_niko(id: int, req: dto.NikoRequest):
+@run_in_session
+def insert_niko(session, req: dto.NikoRequest):
+    stmt = insert(Niko).values(name=req.name, description=req.description,
+    image=req.image, doc="", author=req.author, full_desc=req.full_desc)
+
+    session.execute(stmt)
+    session.commit()
+    return {"msg":"Inserted Niko."}
+
+@run_in_session
+def update_niko(session, id: int, req: dto.NikoRequest):
     entity = session.execute(select(Niko).where(Niko.id == id)).scalar_one_or_none()
     if entity is None:
         return None
@@ -56,37 +69,33 @@ def update_niko(id: int, req: dto.NikoRequest):
     entity.author = req.author
     session.commit()
     return {"msg":"Updated Niko."}
-    
-def delete_niko(id: int):
-    try:
-        entity = session.get(Niko, id)
-        session.delete(entity)
-        session.commit()
-        return {"msg":"Deleted Niko."}
-    except:
-        return None
 
-def get_abilities():
+@run_in_session
+def delete_niko(session, id: int):
+    entity = session.get(Niko, id)
+    session.delete(entity)
+    session.commit()
+    return {"msg":"Deleted Niko."}
+
+@run_in_session
+def get_abilities(session):
     stmt = select(Ability)
     return session.scalars(stmt).fetchall()
 
-def get_ability_by_id(id: int):
-    try:
-        stmt = select(Ability).where(Ability.id == id)
-        return session.scalars(stmt).one()
-    except:
-        return None
+@run_in_session
+def get_ability_by_id(session, id: int):
+    stmt = select(Ability).where(Ability.id == id)
+    return session.scalars(stmt).one()
 
-def insert_ability(req: dto.AbilityRequest):
-    try:
-        stmt = insert(Ability).values(name=req.name, niko_id=req.niko_id)
-        result = session.execute(stmt)
-        session.commit()
-        return {"msg":"Inserted Ability."}
-    except:
-        return None
-    
-def update_ability(id: int, req: dto.AbilityRequest):
+@run_in_session
+def insert_ability(session, req: dto.AbilityRequest):
+    stmt = insert(Ability).values(name=req.name, niko_id=req.niko_id)
+    session.execute(stmt)
+    session.commit()
+    return {"msg":"Inserted Ability."}
+
+@run_in_session
+def update_ability(session, id: int, req: dto.AbilityRequest):
     entity = session.execute(select(Ability).where(Ability.id == id)).scalar_one_or_none()
     if entity is None:
         return None
@@ -94,45 +103,39 @@ def update_ability(id: int, req: dto.AbilityRequest):
     entity.niko_id = req.niko_id
     session.commit()
     return {"msg":"Updated Ability."}
-    
-def delete_ability(id: int):
-    try:
-        entity = session.get(Ability, id)
-        session.delete(entity)
-        session.commit()
-        return {"msg":"Deleted Ability."}
-    except:
-        return None
 
-def get_user_by_username(username: str):
-    try:
-        stmt = select(User).where(User.username == username).limit(1)
-        return session.scalars(stmt).one()
-    except Exception:
-        return None
+@run_in_session
+def delete_ability(session, id: int):
+    entity = session.get(Ability, id)
+    session.delete(entity)
+    session.commit()
+    return {"msg":"Deleted Ability."}
 
-def get_blogs():
+@run_in_session
+def get_user_by_username(session, username: str):
+    stmt = select(User).where(User.username == username).limit(1)
+    return session.scalars(stmt).one()
+
+@run_in_session
+def get_blogs(session, ):
     stmt = select(Blog)
     return session.scalars(stmt).fetchall()
 
-def get_blog_by_id(id: int):
-    try:
-        stmt = select(Blog).where(Blog.id == id).limit(1)
-        return session.scalars(stmt).one()
-    except:
-        return None
+@run_in_session
+def get_blog_by_id(session, id: int):
+    stmt = select(Blog).where(Blog.id == id).limit(1)
+    return session.scalars(stmt).one()
 
-def post_blog(req: dto.BlogRequest):
-    try:
-        stmt = insert(Blog).values(title=req.title, content=req.content,
-            author=req.author, post_datetime=datetime.datetime.now())
-        result = session.execute(stmt)
-        session.commit()
-        return {"msg":"Posted Blog."}
-    except:
-        return None
-    
-def update_blog(id: int, req: dto.BlogRequest):
+@run_in_session
+def post_blog(session, req: dto.BlogRequest):
+    stmt = insert(Blog).values(title=req.title, content=req.content,
+        author=req.author, post_datetime=datetime.datetime.now())
+    session.execute(stmt)
+    session.commit()
+    return {"msg":"Posted Blog."}
+
+@run_in_session
+def update_blog(session, id: int, req: dto.BlogRequest):
     entity = session.execute(select(Blog).where(Blog.id == id)).scalar_one_or_none()
     if entity is None:
         return None
@@ -141,15 +144,13 @@ def update_blog(id: int, req: dto.BlogRequest):
     entity.author = req.author
     session.commit()
     return {"msg":"Updated Blog."}
-    
-def delete_blog(id: int):
-    try:
-        entity = session.execute(select(Blog).where(Blog.id == id)).scalar_one()
-        session.delete(entity)
-        session.commit()
-        return {"msg":"Deleted Blog."}
-    except:
-        return None
+
+@run_in_session
+def delete_blog(session, id: int):
+    entity = session.execute(select(Blog).where(Blog.id == id)).scalar_one()
+    session.delete(entity)
+    session.commit()
+    return {"msg":"Deleted Blog."}
 
 def close_connection():
-    session.close()
+    SessionLocal.close_all()
